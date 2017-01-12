0b968b9f9ce000d3f4b1129cc40fbd3cdd888528
Ivan <Ivan@DESKTOP-FFSE4M7>
2017-01-12 14:13:07 -0800

    Add project files.

---
+++ MVCProject/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCProject.Models
+{
+    public class Student
+    {
+        public int StudentID { get; set; }
+        public string Ime { get; set; }
+        public string Prezime { get; set; }
+    }
+}
